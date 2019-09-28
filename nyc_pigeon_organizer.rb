8c11786bbdb6a9cf5d6f52f0a16eeedbe2aea8ef
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-28 11:29:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,7 +17,7 @@ def nyc_pigeon_organizer(pigeon_data)
             new_pigeon_hash[pigeon_name][attribute].push(att_type.to_s)
             
           else #: Theo exists
-            new_pigeon_hash[pigeon_name] = {}
+            new_pigeon_hash[pigeon_name] = {
           end
         end #loop thru array of pigeon names
       end #loop thru attribute color,gender,lives
