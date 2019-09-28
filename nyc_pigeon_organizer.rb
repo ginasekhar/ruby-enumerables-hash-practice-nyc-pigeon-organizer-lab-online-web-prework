0bc327a9fc8f31d5c353c3b4c860c2846a992e1e
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-28 11:33:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+
+require 'pry'
+def nyc_pigeon_organizer(pigeon_data)
+  
+    new_pigeon_hash = {}
+    pigeon_data.each do |attribute, attribute_hash|
+      attribute_hash.each do |att_type, att_value_array|
+        att_value_array.each do |pigeon_name|
+          #binding.pry
+          if !new_pigeon_hash[pigeon_name]
+             new_pigeon_hash[pigeon_name] = {}
+          end #e.g. Theo exists
+            
+            if !new_pigeon_hash[pigeon_name][attribute]  #:color does not exist
+                new_pigeon_hash[pigeon_name][attribute] = []
+            end #:color exists
+            new_pigeon_hash[pigeon_name][attribute].push(att_type.to_s)
+            
+      
+    
+          
+        end #loop thru array of pigeon names
+      end #loop thru attribute color,gender,lives
+    end #loop thru each input element
+ # binding.pry
+  return new_pigeon_hash  
+  end
